648323e6f89ff2e3cda1bd7c99f9b54d6ce9c711
Ray Fajar S <rayfajars@gmail.com>
2023-04-19 15:06:48 +0700

    feat: add case studies

---
+++ components/Base/Cards/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-[388px] w-[360px] mr-5">
+    <div>
+      <BaseIcon
+        mode="image"
+        :icon="`images/${images}`"
+        width="360"
+        height="280"
+      />
+    </div>
+    <div>
+      <h4 class="text-gray-900 font-bold text-2xl">{{ title }}</h4>
+      <p class="text-gray-700 font-medium text-lg self-stretch">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCards',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    images: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- components/HomePage/Articel/index.vue
+++ components/HomePage/Articel/index.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="container mt-16">
-        <BaseTitle>Case Studies</BaseTitle>
+        <div class="container">
+          <BaseTitle>Case Studies</BaseTitle>
+          <div class="flex mt-10">
+            <BaseCards title="Float Life" description="Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos. Cumq..." images="image1.png"/>
+            <BaseCards title="Retro Call" description="Distinctio aperiam maiores et mollitia necessitatibus. Et reprehenderit." images="image2.png"/>
+          </div>
+
+        </div>
 
     </div>
 </template>
