e2161facc6329b1ce7c757e84cc803185a6c2f0d
Ray Fajar S <rayfajars.com>
2023-04-19 22:10:36 +0700

    feat: add data dummy for slicing

--- components/HomePage/Articel/index.vue
+++ components/HomePage/Articel/index.vue
@@ -1,19 +1,40 @@
 <template>
-    <div class="container mt-16">
-        <div class="container">
-          <BaseTitle>Case Studies</BaseTitle>
-          <div class="flex mt-10">
-            <BaseCards title="Float Life" description="Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos. Cumq..." images="image1.png"/>
-            <BaseCards title="Retro Call" description="Distinctio aperiam maiores et mollitia necessitatibus. Et reprehenderit." images="image2.png"/>
-          </div>
-
-        </div>
-
+  <div class="container mt-16">
+    <div class="container">
+      <BaseTitle>Case Studies</BaseTitle>
+      <div class="flex mt-10">
+        <BaseCards
+          v-for="data in dataCaseStudies"
+          :key="data.title"
+          :title="data.title"
+          :description="data.description"
+          :images="data.images"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomePageArticel',
+  data() {
+    return {
+      dataCaseStudies: [
+        {
+          title: 'Float Life',
+          description:
+            'Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos. Cumq...',
+          images: 'image1.png',
+        },
+        {
+          title: 'Retro Call',
+          description:
+            'Distinctio aperiam maiores et mollitia necessitatibus. Et reprehenderit.',
+          images: 'image2.png',
+        },
+      ],
+    }
+  },
 }
 </script>
--- components/HomePage/RecentWriting/index.vue
+++ components/HomePage/RecentWriting/index.vue
@@ -3,23 +3,19 @@
     <div class="container">
       <BaseTitle>Recent Writing</BaseTitle>
       <BaseTextCardArticel
-        title="Vortals convergence TCP"
-        description="Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos. Cumq..."
-        date="Thu Jan 09, 2031"
+        v-for="data in dataRecentWriting"
+        :key="data.title"
+        :title="data.title"
+        :description="data.description"
+        :date="data.date"
         class="mt-10 mb-[64px]"
-      />
-      <BaseTextCardArticel
-        title="Unbranded navigate"
-        description="Veniam consequatur distinctio. Sit dicta non odit ut. Eum iure dolor in et odit. Consectetur quis ut sit ducimus..."
-        date="Fri Jun 11, 2027"
-        class="mb-[32px]"
       />
 
       <button
-          class="text-black font-bold text-center text-lg h-[48px] w-[234px] border-2 border-solid border-black hover:bg-black hover:text-white hover:border-none transition-colors ease-in-out duration-300"
-        >
+        class="text-black font-bold text-center text-lg h-[48px] w-[234px] border-2 border-solid border-black hover:bg-black hover:text-white hover:border-none transition-colors ease-in-out duration-300"
+      >
         View all Posts
-        </button>
+      </button>
     </div>
   </div>
 </template>
@@ -27,6 +23,24 @@
 <script>
 export default {
   name: 'HomePageRecentWriting',
+  data() {
+    return {
+      dataRecentWriting: [
+        {
+          title: 'Vortals convergence TCP',
+          description:
+            'Iusto labore necessitatibus. Ut minima autem fuga ad eaque eos. Cumq...',
+          date: 'Thu Jan 09, 2031',
+        },
+        {
+          title: 'Unbranded navigate',
+          description:
+            'Veniam consequatur distinctio. Sit dicta non odit ut. Eum iure dolor in et odit. Consectetur quis ut sit ducimus...',
+          date: 'Fri Jun 11, 2027',
+        },
+      ],
+    }
+  },
 }
 </script>
 
